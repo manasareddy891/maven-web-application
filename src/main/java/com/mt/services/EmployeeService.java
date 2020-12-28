de185784140b8996cbfea892e53c1cf78f6ea96d
manasareddy891 <73627857+manasareddy891@users.noreply.github.com>
2020-12-28 15:19:25 -0600

    Update EmployeeService.java

--- src/main/java/com/mt/services/EmployeeService.java
+++ src/main/java/com/mt/services/EmployeeService.java
@@ -30,7 +30,7 @@ public class EmployeeService {
 		js.put("Hobbies", "Reading Technical Blogs,Teaching, Helping to Poor People..");
 		js.put("Places he like", "His native place");
 
-		System.out.println("Hello leaving Employee Service");
+		System.out.println("Hello leaving Employee Service"+js);
 		
 		return js.toString();
 }
